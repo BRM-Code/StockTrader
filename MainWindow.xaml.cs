a361ede801c3267a5962fd98a3aca5c4191cda5a
Ben Rowberry <mrowberryb@gmail.com>
2020-02-11 13:53:43 +0000

    Added a Stack panel with the diffrent companies on

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,6 +29,87 @@ namespace StockTrader_.NET_Framework_
             var values = ApiCommunicator.CollectData("aapl");
             GraphHandler LineGraph = new GraphHandler(linegraph);
             LineGraph.Draw(values);
+
+        }
+
+        private void Alphabet(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Microsoft(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Apple(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Uber(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Intel(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void IBM(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Facebook(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WD(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Nvidia(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Oracle(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Amazon(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AMD(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Dell(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Adobe(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void QinetiQ(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Spotify(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
